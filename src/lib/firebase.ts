cb5ef3d8622dd357fdbf2bc153bf6cf22eeab60a
Super Admin <admin@jamesknight.altostrat.com>
2025-08-29 21:07:43 +0000

    No undo that, its not the domain or anything. Its just a databaseId when

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "cbre-poc.firebaseapp.com",
-  projectId: "cbre-poc",
-  storageBucket: "cbre-poc.appspot.com",
+  authDomain: "checkmapper.firebaseapp.com",
+  projectId: "checkmapper",
+  storageBucket: "checkmapper.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+const db = getFirestore(app, "cbre-poc");
 
 export { app, db };
